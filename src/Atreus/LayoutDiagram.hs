0d51edffe494196d7d5f34b0c82668081839fb9b
Mr. Fluffy <git@sixearsm>
2020-10-29 18:50:43 +0000

    list of keys

--- src/Atreus/LayoutDiagram.hs
+++ src/Atreus/LayoutDiagram.hs
@@ -14,7 +14,8 @@ import Prelude  ( Double, RealFloat )
 
 -- base --------------------------------
 
-import Control.Monad           ( return )
+import Control.Monad           ( mapM, return )
+import Control.Monad.IO.Class  ( MonadIO, liftIO )
 import Data.Bool               ( Bool( False ) )
 import Data.Function           ( ($), flip )
 import Data.Monoid             ( mconcat )
@@ -35,12 +36,12 @@ import Diagrams.Core.Transform  ( transform )
 
 -- diagrams-lib ------------------------
 
-import Diagrams.Angle             ( (@@), deg, rotation )
+import Diagrams.Angle             ( Angle, (@@), deg, rotation )
 import Diagrams.Attributes        ( lw, none )
 import Diagrams.TwoD.Align        ( alignBL, alignBR, alignTL, alignTR
                                   , centerXY )
 import Diagrams.TwoD.Attributes   ( fc )
-import Diagrams.TwoD.Combinators  ( (|||) )
+import Diagrams.TwoD.Combinators  ( vcat )
 import Diagrams.TwoD.Path         ( strokeP )
 import Diagrams.TwoD.Shapes       ( roundedRect )
 import Diagrams.TwoD.Types        ( V2( V2 ) )
@@ -73,52 +74,86 @@ type 𝕊 = String
 
 data Fonts ν = Fonts { lin2 ∷ PreparedFont ν }
 
-------------------------------------------------------------
-
-box ∷ Diagram B
-box = roundedRect 1 1 0.05
-
-topts_ ∷ MonadReader (Fonts ν) η ⇒ ν → ν → η (TextOpts ν)
-topts_ w h = do
-  l2 ← asks lin2
-  return TextOpts { textFont = l2, mode = INSIDE_H, spacing = KERN
-                  , underline = False, textWidth = w, textHeight = h }
-
+{- | Read in a Fonts datum -}
 getFonts ∷ (Read ν, RealFloat ν) ⇒ IO (Fonts ν)
 getFonts = do
   l2 ← SF.lin2
   return $ Fonts { lin2 = l2 }
 
-ss ∷ TextOpts 𝔻 → 𝕊 → Diagram B
-ss o t = strokeP $ textSVG' o t
+------------------------------------------------------------
 
-xx' ∷ MonadReader (Fonts 𝔻) η ⇒ 𝔻 → 𝔻 → 𝕊 → η (Diagram B)
-xx' h w t = do
-  o ← topts_ w h
-  return $ ss o t # lw none
+{- | A width-one square with slightly rounded corners. -}
+box1 ∷ Diagram B
+box1 = roundedRect 1 1 0.05
 
-tx'' ∷ MonadReader (Fonts 𝔻) η ⇒
-      𝔻 → 𝔻 → 𝕊 → Colour 𝔻 → 𝔻 → 𝔻 → (Diagram B → Diagram B) → η (Diagram B)
-tx'' h w t c x y a = do
-  t' ← xx' h w t
+----------------------------------------
+
+{- | Create a `TextOpts` datum with given height; using the `SF.lin2`
+     font, with the given sizing mode, height & width; using Kerning, and no
+     underline.
+ -}
+topts ∷ MonadReader (Fonts ν) η ⇒ Mode → ν → ν → η (TextOpts ν)
+topts m h w = do
+  l2 ← asks lin2
+  return TextOpts { textFont = l2, mode = m, spacing = KERN
+                  , underline = False, textWidth = w, textHeight = h }
+
+----------------------------------------
+
+{- | Create a text diagram of given height (using the `SF.lin2` font). -}
+text ∷ MonadReader (Fonts 𝔻) η ⇒ 𝔻 → 𝕊 → η (Diagram B)
+text h t = do
+  o ← topts INSIDE_H h 1 -- the width is irrelevant with INSIDE_H
+  return $ strokeP (textSVG' o t) # lw none
+
+----------------------------------------
+
+{- | Create a text diagram of given height (using the `SF.lin2` font);
+     with given height, colour, alignment; and position. -}
+text' ∷ MonadReader (Fonts 𝔻) η ⇒
+      𝔻 → 𝕊 → Colour 𝔻 → 𝔻 → 𝔻 → (Diagram B → Diagram B) → η (Diagram B)
+text' h  t c x y a = do
+  t' ← text h t
   return (moveOriginBy (V2 x y) $ t' # fc c # a)
 
-tx' ∷ MonadReader (Fonts 𝔻) η ⇒
-      𝕊 → Colour 𝔻 → 𝔻 → 𝔻 → (Diagram B → Diagram B) → η (Diagram B)
-tx' = tx'' 0.35 0.5
+----------------------------------------
+
+{- | Create a diagram for a key with the given labels. -}
+key ∷ MonadIO μ ⇒ (𝕊,𝕊,𝕊,𝕊,𝕊) → μ (Diagram B)
+key (tl,tr,c,bl,br) = liftIO $ do
+  fonts ← getFonts
+  flip runReaderT fonts $ do
+    t0' ← text' 0.5  c  grey    0       0     centerXY
+    t1' ← text' 0.35 tr red   (-0.45) (-0.45) alignTR
+    t2' ← text' 0.35 br green (-0.45)   0.45  alignBR
+    t3' ← text' 0.35 tl blue    0.45  (-0.45) alignTL
+    t4' ← text' 0.35 bl yellow  0.45    0.45  alignBL
+
+    return $ mconcat [ box1, t0', t1', t2', t3', t4' ]
+
+key_rot ∷ MonadIO μ ⇒ Angle 𝔻 → (𝕊,𝕊,𝕊,𝕊,𝕊) → μ (Diagram B)
+key_rot r ts = do
+  k ← key ts
+  return $ k # transform (rotation r)
+
+layout ∷ [(𝕊,𝕊,𝕊,𝕊,𝕊)]
+layout = [ ("Q", "Ins", "!", "", "")
+         , ("A", "Del", "(", "", "")
+         , ("Z", "", "[", "", "")
+         ]
 
 atreus_layout ∷ IO (Diagram B)
 atreus_layout = do
   fonts ← getFonts
   flip runReaderT fonts $ do
-    t0' ← tx'' 0.5 0.5 "Q" grey 0 0 centerXY
-    t1' ← tx' "PgUp" red (-0.45) (-0.45) alignTR
-    t2' ← tx' "Ins" green (-0.45) 0.45 alignBR
-    t3' ← tx' "7" blue 0.45 (-0.45) alignTL
-    t4' ← tx' "TAB" yellow 0.45 0.45 alignBL
+    t0' ← text' 0.5  "Q"    grey    0       0     centerXY
+    t1' ← text' 0.35 "PgUp" red   (-0.45) (-0.45) alignTR
+    t2' ← text' 0.35 "Ins"  green (-0.45)   0.45  alignBR
+    t3' ← text' 0.35 "7"    blue    0.45  (-0.45) alignTL
+    t4' ← text' 0.35 "TAB"  yellow  0.45    0.45  alignBL
 
-    return $
-          mconcat [ box, t0', t1', t2', t3', t4' ] # transform (rotation $ -10@@deg)
-      ||| (mconcat [ box, t0', t1', t2', t3', t4' ]) # transform (rotation $ -10@@deg)
-
+    k ← key_rot (-10@@deg) ("fee", "fi", "fo", "fum", "English")
+    ks ← mapM (key_rot (-10@@deg)) layout
+    return $ vcat ks
+   
 -- that's all, folks! ----------------------------------------------------------
