be86ddf4a9d1e27c762af1d6ffb1d82ba19a976f
Mr. Fluffy <git@sixearsm>
2020-12-17 06:35:41 +0000

    better options

--- bin/atreus.hs
+++ bin/atreus.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE UnicodeSyntax     #-}
 
+import Prelude  ( fromIntegral )
+
 -- base --------------------------------
 
-import Control.Applicative     ( (<**>), some )
-import Control.Monad           ( (>>=) )
-import Control.Monad.IO.Class  ( liftIO )
-import Data.Function           ( ($) )
-import Data.Functor            ( (<$>) )
-import System.IO               ( FilePath, IO )
+import Control.Applicative  ( (<*>), (<**>), some )
+import Data.Function        ( ($) )
+import Data.Functor         ( (<$>) )
+import System.IO            ( FilePath, IO )
 
 -- base-unicode-symbols ----------------
 
-import Data.Function.Unicode  ( (∘) )
-import Data.Monoid.Unicode    ( (⊕) )
+import Data.Monoid.Unicode      ( (⊕) )
+import Numeric.Natural.Unicode  ( ℕ )
 
 -- diagrams-lib ------------------------
 
@@ -21,13 +21,14 @@ import Diagrams.TwoD.Size  ( mkWidth )
 
 -- diagrams-svg ------------------------
 
-import Diagrams.Backend.SVG          ( renderPretty )
-import Diagrams.Backend.SVG.CmdLine  ( mainWith )
+import Diagrams.Backend.SVG  ( renderPretty )
 
 -- optparse-applicative ----------------
 
-import Options.Applicative.Builder  ( fullDesc, header, info, metavar, progDesc
-                                    , strArgument )
+import Options.Applicative.Builder  ( auto, fullDesc, info, long, metavar
+                                    , option, progDesc, short, strArgument
+                                    , strOption, value
+                                    )
 import Options.Applicative.Extra    ( execParser, helper )
 import Options.Applicative.Types    ( Parser, ParserInfo )
 
@@ -39,19 +40,24 @@ import Atreus.LayoutDiagram  ( atreus_layout )
 
 --------------------------------------------------------------------------------
 
-data Options = Options { layerFns ∷ [FilePath] }
+data Options = Options { layerFns ∷ [FilePath]
+                       , outputFn ∷ FilePath
+                       , width    ∷ ℕ
+                       }
 
 parseOpts ∷ Parser Options
 parseOpts = Options <$> some (strArgument $ metavar "LAYER-FILE+")
+                    <*> strOption (short 'o' ⊕ long "output"
+                                             ⊕ metavar "OUTPUT-FILE")
+                    <*> option auto (short 'w' ⊕ value 400)
 
 main ∷ IO ()
 main = do
-  let parser ∷ ParserInfo Options
-      parser = info (parseOpts <**> helper)
-                    (fullDesc ⊕ progDesc "Print a greeting for TARGET"
-                              ⊕ header "hello - a test for optparse" )
+  let prog_desc = progDesc "create an atreus layout svg from layer descriptions"
+      parser ∷ ParserInfo Options
+      parser = info (parseOpts <**> helper) (fullDesc ⊕ prog_desc)
   opts ← execParser parser
---  liftIO ∘ mainWith $ atreus_layout (layerFns opts)
-  atreus_layout (layerFns opts) >>= renderPretty "atreus.svg" (mkWidth 400)
+  dia ← atreus_layout (layerFns opts)
+  renderPretty (outputFn opts) (mkWidth (fromIntegral $ width opts)) dia
 
 -- that's all, folks! ----------------------------------------------------------
